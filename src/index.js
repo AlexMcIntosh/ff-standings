d1271fec68ecd438f39277c16da43c851c1bf64e
Alex McIntosh <alexjm.tx@gmail.com>
2024-10-09 21:51:44 -0700

    calc matchup points

--- src/index.js
+++ src/index.js
@@ -1,38 +1,61 @@
 import { getLeagueMatchups, getLeagueUsers, getRosters } from "./sleeper.js";
+import { calcPayout } from "./utils.js";
 
 const LEAGUE_ID = "1124850847474282496";
+const CURR_WEEK = 7;
 
 const matchups = await getLeagueMatchups(LEAGUE_ID, 1);
-const userMap = new Map();
 const users = await getLeagueUsers(LEAGUE_ID);
 const rosters = await getRosters(LEAGUE_ID);
 
-users.forEach((user, index) => {
-    userMap.set(user.user_id, { "name": user.display_name })
+const rosterMap = new Map();
+const userMap = new Map();
+
+users.forEach((user) => {
+    userMap.set(user.user_id, { "display_name": user.display_name })
 });
 
-// matchups.forEach((matchup, index) => {
-//     console.log(matchup)
-// });
-
-rosters.forEach((roster, index) => {
+rosters.forEach((roster) => {
     // delete the co_owners
     if (roster.co_owners) {
         roster.co_owners.forEach((id) => {
-            userMap.delete(id);
+            if (userMap.has(id)) {
+                userMap.delete(id);
+            }
         })
     }
 
+    // roster may be a co owner roster but we dont want to populate the map with them
     if (userMap.has(roster.owner_id)) {
-        const value = userMap.get(roster.owner_id);
+        const user = userMap.get(roster.owner_id);
         const { wins, losses } = roster.settings;
-        value.wins = wins;
-        value.losses = losses;
-        value.payout = wins * 25;
-        userMap.set(roster.owner_id, value);
+        const payout = calcPayout(wins);
+        rosterMap.set(roster.roster_id, {
+            wins,
+            losses,
+            payout,
+            "display_name": user.display_name,
+            "owner_id": roster.owner_id
+        });
     }
-})
+});
 
-userMap.forEach((value, key, map) => {
-    console.log(value)
+// sort matchups in descending order by points
+matchups.sort((a, b) => {
+    return a.points - b.points;
+});
+
+matchups.forEach((matchup, index) => {
+    if(rosterMap.has(matchup.roster_id)) {
+        const roster = rosterMap.get(matchup.roster_id);
+        const points = index + (roster?.points ?? 0);
+        rosterMap.set(matchup.roster_id, {
+            ...roster,
+            points
+        })
+    }
+});
+
+rosterMap.forEach((roster) => {
+    console.log(roster);
 })
---
+++ src/utils.js
@@ -0,0 +1,4 @@
+export const calcPayout = (wins) => {
+    const payout = 25;
+    return wins * payout;
+}
