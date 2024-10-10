877694b5deca5377924d4e24f587c7a104a20ae6
Alex McIntosh <alexjm.tx@gmail.com>
2024-10-09 22:11:23 -0700

    generate final standings

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import { getLeagueMatchups, getLeagueUsers, getRosters } from "./sleeper.js";
 import { calcPayout } from "./utils.js";
 
 const LEAGUE_ID = "1124850847474282496";
-const CURR_WEEK = 7;
+const CURR_WEEK = 5;
 
-const matchups = await getLeagueMatchups(LEAGUE_ID, 1);
 const users = await getLeagueUsers(LEAGUE_ID);
 const rosters = await getRosters(LEAGUE_ID);
 
 const rosterMap = new Map();
 const userMap = new Map();
+
+const getAndSortPtsAscMatchupForWeek = async (week) => {
+    const matchupForWeek = await getLeagueMatchups(LEAGUE_ID, week);
+
+    // sort matchups in descending order by points
+    matchupForWeek.sort((a, b) => {
+        return a.points - b.points;
+    });
+
+    return matchupForWeek;
+};
 
 users.forEach((user) => {
     userMap.set(user.user_id, { "display_name": user.display_name })
@@ -40,22 +50,29 @@ rosters.forEach((roster) => {
     }
 });
 
-// sort matchups in descending order by points
-matchups.sort((a, b) => {
-    return a.points - b.points;
-});
+for(let i = 1; i <= CURR_WEEK; i++) {
+    const sortedMatchups = await getAndSortPtsAscMatchupForWeek(i);
+    sortedMatchups.forEach((matchup, index) => {
+        if(rosterMap.has(matchup.roster_id)) {
+            const roster = rosterMap.get(matchup.roster_id);
+            const points = index + (roster?.points ?? 0);
+            rosterMap.set(matchup.roster_id, {
+                ...roster,
+                points
+            })
+        }
+    });
+}
 
-matchups.forEach((matchup, index) => {
-    if(rosterMap.has(matchup.roster_id)) {
-        const roster = rosterMap.get(matchup.roster_id);
-        const points = index + (roster?.points ?? 0);
-        rosterMap.set(matchup.roster_id, {
-            ...roster,
-            points
-        })
-    }
-});
-
+const finalStandings = [];
 rosterMap.forEach((roster) => {
-    console.log(roster);
-})
+    finalStandings.push(roster);
+});
+
+finalStandings.sort((a, b) => {
+    return b.points - a.points;
+});
+
+finalStandings.forEach((team) => {
+    console.log(team);
+});
